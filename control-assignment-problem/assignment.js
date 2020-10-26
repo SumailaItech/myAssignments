f37cb0d09d1ea47abf998569593349d608717628
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-26 18:03:50 +0200

    I have finished setting up the assigments file and has gone futher to  create an if statement to show an alert message when the random number is more than 0.7 and also created  an array and a for loops to loop through the contents of the arrays

--- control-assignment-problem/assignment.js
+++ control-assignment-problem/assignment.js
@@ -3,3 +3,12 @@ const randomNumber = Math.random(); // produces random number between 0 (includi
 if(randomNumber > 0.7){
     alert('Your number is more than 0.7');
 }
+
+let arrayNumbers = [1,2,3,4,5,6,7,8,9];
+for(let i = 0; i < arrayNumbers.length; i++){
+    console.log(arrayNumbers[i]);
+}
+
+for(const elm of arrayNumbers){
+    console.log(elm);
+}
