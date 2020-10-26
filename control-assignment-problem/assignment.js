51ee66f9d8d97d4e3d3a656174bd8383a9082527
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-26 18:25:25 +0200

    created another random number and used logical operators to check wherether the numbers genearted are greater than 0.7 and less than 0.2

--- control-assignment-problem/assignment.js
+++ control-assignment-problem/assignment.js
@@ -1,7 +1,18 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+const anotherRandomNumber = Math.random();
 
-if(randomNumber > 0.7){
-    alert('Your number is more than 0.7');
+// if(randomNumber > 0.7){
+//     alert('Your number is more than 0.7');
+// }
+
+if(randomNumber > 0.7 && anotherRandomNumber > 0.7){
+    alert('Both numbers are greater that 0.7');
+    console.log(randomNumber +','+ anotherRandomNumber);
+}else if(randomNumber < 0.2 || anotherRandomNumber < 0.2){
+    alert("one of the numbers is less than 0.2");
+    console.log(randomNumber +','+ anotherRandomNumber);
+}else{
+    alert("None Much");
 }
 
 let arrayNumbers = [1,2,3,4,5,6,7,8,9];
@@ -13,3 +24,6 @@ for(let i = arrayNumbers.length; i >= 0; i--){
 for(const elm of arrayNumbers){
     console.log(elm);
 }
+
+
+
