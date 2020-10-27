0e0ccaa3ecbab1816cbf626e7f23a1edcfe696ad
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-27 23:36:49 +0200

    Created three function with different syntaxes

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,9 @@
-const sayHello = (name)=> {
-  console.log('Hi ' + name);
-}
+const sayHello = name => console.log('Hi ' + name);
+const sayHello1 =(name, phrase) => console.log(phrase+' '+name);
+const sayHello2 = ()=>console.log('I am still learning the javascript');
+const sayHello3 = name => 'Hello ' + name;
 
-sayHello();
+sayHello('Sumaila');
+sayHello1('Sumaila','Hi');
+sayHello2();
+console.log(sayHello3('Sumaila'));
