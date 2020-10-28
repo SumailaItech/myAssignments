885cd0131fab60756e3fca874c1369b94e2a3453
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-28 02:32:08 +0200

    Selected task one and change the text color and the background color,so am done with task 1

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,7 @@
+const task1 = document.getElementById('task-1');
+task1.style.color = 'white';
+task1.style.backgroundColor ='#000';
+
+const taskOne = document.querySelector('#task-1');
+taskOne.style.color ='#fff';
+taskOne.style.backgroundColor ='#000';
