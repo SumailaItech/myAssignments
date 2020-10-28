77921ba2adfcef5de6408c574d923c3c658cfb48
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-29 00:21:46 +0200

    Successfully created the findMax function and has return the max and min using destructuring  value from the array

--- arrays-assignment-problem/assignment.js
+++ arrays-assignment-problem/assignment.js
@@ -1,3 +1,4 @@
+// Task one
 const arrNumber =[3, 5, 6, 7,4,8,9,21];
 
 const filterNum = arrNumber.filter(num=>num > 5);
@@ -14,10 +15,22 @@ console.log(mapNum);
 const reduceNum = arrNumber.reduce((pValue,cValue)=>pValue + cValue);
 console.log(reduceNum)
 
+// Task two
 const findMax = (...arrNumber) =>{
     const maxNum = Math.max(...arrNumber);
+    
     return maxNum;
 }
 
 const maxNum = findMax(...arrNumber);
-console.log(maxNum);
+
+// task three
+
+const findMax1 = (...arrNumber) =>{
+    const maxNum = Math.max(...arrNumber);
+    const minNum = Math.min(...arrNumber);
+    return [maxNum, minNum];
+}
+
+const [maxNum1, minNum] = findMax1(...arrNumber);
+console.log('Max is '+maxNum1 +' and min is '+minNum);
