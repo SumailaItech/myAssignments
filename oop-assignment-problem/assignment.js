09120c7fedcbfaca6178fbe29aad737f0250f0e9
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-30 00:14:14 +0200

    I have created the clas Course and instantiated twice and console log the result,so done with task one

--- oop-assignment-problem/assignment.js
+++ oop-assignment-problem/assignment.js
@@ -1,5 +1,12 @@
 class Course{
-    title;
-    length;
-    price;
+    constructor(title,length,price){
+        this.title = title;
+        this.length = length;
+        this.price = price;
+    }
 }
+
+const math = new Course('math101',52,10);
+console.log(math);
+const chem = new Course('chem101',23,20);
+console.log(chem);
