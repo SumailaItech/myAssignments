01f037827554c4719bf00f249adf424e7c3bfa4b
iddrisu Sumaila <iddrisu.sumaila@amalitech.org>
2020-10-30 00:25:36 +0200

    I have added two methods to the class and output the results in the logs

--- oop-assignment-problem/assignment.js
+++ oop-assignment-problem/assignment.js
@@ -4,9 +4,24 @@ class Course{
         this.length = length;
         this.price = price;
     }
+
+    lengthPerPrice(){
+        return this.lengthPerPrice = this.length/this.price;
+    }
+
+    summary(){
+        const bookSummary =`
+        The Summary of the Book
+        title: ${this.title},
+        Length: ${this.length},
+        Price: ${this.price}
+        `;
+        return bookSummary;
+    }
 }
 
 const math = new Course('math101',52,10);
-console.log(math);
+console.log(math.lengthPerPrice());
+console.log(math.summary());
 const chem = new Course('chem101',23,20);
 console.log(chem);
